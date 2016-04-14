a492e8a365cd071e45fd16b82cb78046489df5e2
Scisco <scisco7@gmail.com>
2016-04-14 12:31:02 -0400

    add file and s3 writer

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
+import json
 import boto3
 import click
 import logging
 from copy import copy
 from collections import OrderedDict
 from datetime import datetime, date, timedelta
-from sentinel_s3 import range_metadata, s3_writer, single_metadata
+from sentinel_s3 import range_metadata, single_metadata
 
 from elasticsearch import Elasticsearch, RequestError
 
@@ -36,23 +37,38 @@ def create_index(index_name, doc_type):
     )
 
 
+def meta_constructor(metadata):
+    internal_meta = copy(metadata)
+
+    scene_id = 'S2A_tile_{0}_{1}{2}{3}_{4}'.format(
+        metadata['date'].replace('-', ''),
+        metadata['utm_zone'],
+        metadata['latitude_band'],
+        metadata['grid_square'],
+        int(metadata['path'].split('/')[-1])
+    )
+
+    body = OrderedDict({
+        'scene_id': scene_id,
+        'original_scene_id': internal_meta.pop('tile_name'),
+        'satellite_name': metadata.get('spacecraft_name', 'Sentintel-2A'),
+        'cloud_coverage': metadata.get('cloudy_pixel_percentage', 100),
+    })
+
+    body.update(internal_meta)
+
+    try:
+        body['data_geometry'] = body.pop('tile_data_geometry')
+    except KeyError:
+        pass
+
+    return body
+
+
 def elasticsearch_updater(product_dir, metadata):
 
     try:
-        internal_meta = copy(metadata)
-
-        body = OrderedDict({
-            'scene_id': internal_meta.pop('tile_name'),
-            'satellite_name': metadata.get('spacecraft_name', 'Sentintel-2A'),
-            'cloud_coverage': metadata.get('cloudy_pixel_percentage', 100),
-        })
-
-        body.update(internal_meta)
-
-        try:
-            body['data_geometry'] = body.pop('tile_data_geometry')
-        except KeyError:
-            pass
+        body = meta_constructor(metadata)
 
         try:
             es.index(index="satellites", doc_type="sentinel2", id=body['scene_id'],
@@ -64,6 +80,30 @@ def elasticsearch_updater(product_dir, metadata):
     except Exception as e:
         print('Unhandled error occured while writing to elasticsearch')
         print('Details: %s' % e.__str__())
+
+
+def file_writer(product_dir, metadata):
+    body = meta_constructor(metadata)
+
+    f = open(os.path.join(product_dir, body['scene_id'] + '.json'), 'w')
+    f.write(json.dumps(body))
+    f.close()
+
+
+def s3_writer(product_dir, metadata):
+    # make sure product_dir doesn't start with slash (/) or dot (.)
+    if product_dir.startswith('.'):
+        product_dir = product_dir[1:]
+
+    if product_dir.startswith('/'):
+        product_dir = product_dir[1:]
+
+    body = meta_constructor(metadata)
+
+    key = os.path.join(product_dir, body['tile_name'] + '.json')
+    s3.Object(bucket_name, key).put(json.dumps(body))
+    object_acl = s3.ObjectAcl(bucket_name, key)
+    object_acl.put(ACL='public-read')
 
 
 def last_updated(today):
@@ -126,22 +166,24 @@ def geometry_check(meta):
 
 
 @click.command()
-@click.argument('ops', metavar='<operations: choices: s3 | es>', nargs=-1)
+@click.argument('ops', metavar='<operations: choices: s3 | es | disk>', nargs=-1)
 @click.option('--product', default=None, help='Product name. If given only the given product is processed.')
 @click.option('--start', default=None, help='Start Date. Format: YYYY-MM-DD')
 @click.option('--end', default=None, help='End Date. Format: YYYY-MM-DD')
 @click.option('--concurrency', default=20, type=int, help='Process concurrency. Default=20')
 @click.option('--es-host', default='localhost', help='Elasticsearch host address')
 @click.option('--es-port', default=9200, type=int, help='Elasticsearch port number')
+@click.option('--folder', default='.', help='Destination folder if is written to disk')
 @click.option('-v', '--verbose', is_flag=True)
-def main(ops, product, start, end, concurrency, es_host, es_port, verbose):
+def main(ops, product, start, end, concurrency, es_host, es_port, folder, verbose):
 
     if not ops:
         raise click.UsageError('No Argument provided. Use --help if you need help')
 
     accepted_args = {
         'es': elasticsearch_updater,
-        's3': s3_writer
+        's3': s3_writer,
+        'disk': file_writer
     }
 
     writers = []
@@ -165,7 +207,7 @@ def main(ops, product, start, end, concurrency, es_host, es_port, verbose):
     logger.addHandler(ch)
 
     if product:
-        single_metadata(product, '.', writers=writers, geometry_check=geometry_check)
+        single_metadata(product, folder, writers=writers)
     else:
 
         if end:
@@ -188,7 +230,7 @@ def main(ops, product, start, end, concurrency, es_host, es_port, verbose):
 
             create_index('satellites', 'sentinel2')
 
-        range_metadata(start, end, '.', concurrency, writers, geometry_check=geometry_check)
+        range_metadata(start, end, folder, concurrency, writers, geometry_check=geometry_check)
 
 
 if __name__ == '__main__':
