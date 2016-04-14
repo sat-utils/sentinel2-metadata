cc16ab1c3a253f5d68541d72cf7a123fd5137168
Scisco <scisco7@gmail.com>
2016-04-14 15:30:25 -0400

    change index name

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from elasticsearch import Elasticsearch, RequestError
 
 bucket_name = os.getenv('BUCKETNAME', 'sentinel-meta')
 s3 = boto3.resource('s3')
+es_index = 'sat-api'
 
 
 def create_index(index_name, doc_type):
@@ -74,11 +75,11 @@ def elasticsearch_updater(product_dir, metadata):
         body = meta_constructor(metadata)
 
         try:
-            es.index(index="satellites", doc_type="sentinel2", id=body['original_scene_id'],
+            es.index(index="sat-api", doc_type="sentinel2", id=body['scene_id'],
                      body=body)
         except RequestError:
             body['data_geometry'] = None
-            es.index(index="satellites", doc_type="sentinel2", id=body['original_scene_id'],
+            es.index(index="sat-api", doc_type="sentinel2", id=body['scene_id'],
                      body=body)
     except Exception as e:
         print('Unhandled error occured while writing to elasticsearch')
@@ -227,7 +228,7 @@ def main(ops, product, start, end, concurrency, es_host, es_port, folder, verbos
                 'port': es_port
             }])
 
-            create_index('satellites', 'sentinel2')
+            create_index(es_index, 'sentinel2')
 
         range_metadata(start, end, folder, concurrency, writers, geometry_check=geometry_check)
 
